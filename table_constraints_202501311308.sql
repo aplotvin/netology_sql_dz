d68e36bd6636ab0ee2a56f4d97850aec95a661d2
aplotvin <163526858+aplotvin@users.noreply.github.com>
2025-01-31 13:10:58 +0300

    Add files via upload

---
+++ table_constraints_202501311308.sql
@@ -0,0 +1,17 @@
+INSERT INTO information_schema.table_constraints (table_name,constraint_name) VALUES
+	 ('actor','actor_pkey'),
+	 ('address','address_pkey'),
+	 ('category','category_pkey'),
+	 ('city','city_pkey'),
+	 ('country','country_pkey'),
+	 ('customer','customer_pkey'),
+	 ('film_actor','film_actor_pkey'),
+	 ('film_category','film_category_pkey'),
+	 ('film','film_pkey'),
+	 ('inventory','inventory_pkey');
+INSERT INTO information_schema.table_constraints (table_name,constraint_name) VALUES
+	 ('language','language_pkey'),
+	 ('payment','payment_pkey'),
+	 ('rental','rental_pkey'),
+	 ('staff','staff_pkey'),
+	 ('store','store_pkey');
